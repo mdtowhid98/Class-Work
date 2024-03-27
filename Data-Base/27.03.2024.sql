231732bc5002c69372303528f7bd5e6db0fe446f
Md Towhidul Alam <alammdtowhidul9@gmail.com>
2024-03-27 16:29:49 +0600

    270.3.2024

--- Data-Base/27.03.2024.sql
+++ Data-Base/27.03.2024.sql
@@ -1,3 +1,10 @@
-select FIRST_NAME
-from employees
-where substr (FIRST_NAME,1,3)='Sun'; 
+SELECT FIRST_NAME,salary,nvl(salary,0),(salary*12)+(salary*12*nvl(commission_pct,0))an_sal
+from employees;
+
+SELECT FIRST_NAME,salary,commission_pct,
+nvl2(commission_pct,salary*commission_pct,salary+2000)income
+from employees;
+
+SELECT FIRST_NAME,last_name,salary,commission_pct,
+nullif(length (FIRST_NAME),length(last_name))result
+from employees;
